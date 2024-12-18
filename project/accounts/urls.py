f2c0e430724c46f74476730705c3aa15d5a649b8
Himanshu72060 <himanshusharma00898@gmail.com>
2024-12-18 11:13:59 +0530

    ok

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from accounts.views import UserRegistrationView, UserLoginView, UserProfileView, UserChangePasswordView, SendPasswordResetEmailView, UserPasswordResetView
 urlpatterns = [
-    path('ragister/', UserRegistrationView.as_view(), name="register"),
+    path('register/', UserRegistrationView.as_view(), name="register"),
     path('login/', UserLoginView.as_view(), name="login"),
     path('profile/', UserProfileView.as_view(), name="profile"),
     path('changepass/', UserChangePasswordView.as_view(), name="changepass"),
     path('send_reset_password_email/', SendPasswordResetEmailView.as_view(),
          name="send_reset_password_email"),
-    path('reset_password/<uid>/<token>/', UserPasswordResetView.as_view(), name="reset_password"),
+    path('reset_password/<uid>/<token>/',
+         UserPasswordResetView.as_view(), name="reset_password"),
 ]
